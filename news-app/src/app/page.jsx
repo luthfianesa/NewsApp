cd15429f9c68458d347cbea10ce347eeec0f63f1
luthfianesa <lutvheanesa@gmail.com>
2024-01-13 12:50:43 +0700

    Add responsive navbar

---
+++ news-app/src/app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="homepage">
+      <h1>This is text from homepage</h1>
+    </main>
+  );
+}
---
+++ news-app/src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import Image from "next/image";
+import NewsBrand from "../../public/images/binar-logo.png";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="navbar-brand">
+        <Image src={NewsBrand} width={50} height={50} alt="news logo" />
+        <h1>Binar News</h1>
+      </div>
+      <ul>
+        <Link href="/">
+          <li>Home</li>
+        </Link>
+        <Link href="/about">
+          <li>About</li>
+        </Link>
+        <Link href="/contact">
+          <li>Contact</li>
+        </Link>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
